77f1dcd608afca65688b1b99cf4db93847b7e4ff
Drew Gillson <drew.gillson@looker.com>
2019-01-03 15:58:45 -0700

    Copied /build to /docs to deploy on Github Pages

---
+++ docs/precache-manifest.26e9c3479ddd9cc3cd2ffd73c975948f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c35b2089d33eb7a6da51834b007a69a3",
+    "url": "/guru-graphql-client/static/media/logo.c35b2089.svg"
+  },
+  {
+    "revision": "9bb77a173b571296dece",
+    "url": "/guru-graphql-client/static/js/runtime~main.9bb77a17.js"
+  },
+  {
+    "revision": "a6ecdd419288c0ed1ae0",
+    "url": "/guru-graphql-client/static/js/main.a6ecdd41.chunk.js"
+  },
+  {
+    "revision": "aa37c57a1adee0abb91b",
+    "url": "/guru-graphql-client/static/js/1.aa37c57a.chunk.js"
+  },
+  {
+    "revision": "a6ecdd419288c0ed1ae0",
+    "url": "/guru-graphql-client/static/css/main.b44ae3cc.chunk.css"
+  },
+  {
+    "revision": "aa37c57a1adee0abb91b",
+    "url": "/guru-graphql-client/static/css/1.6100524d.chunk.css"
+  },
+  {
+    "revision": "8c634e8274b904e5de8d21107c57ea06",
+    "url": "/guru-graphql-client/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c40607968908f5bf2decc4e1aa43096.js"
+  "/guru-graphql-client/precache-manifest.26e9c3479ddd9cc3cd2ffd73c975948f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/guru-graphql-client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
