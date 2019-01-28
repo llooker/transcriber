44017a70f2a4ef1961d2baf9046793c1967dc635
Drew Gillson <drew.gillson@looker.com>
2019-01-28 16:49:19 -0700

    A group of specifically curated cards can now be associated with a Review Type

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,24 +1,85 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo'
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
-const CardList = ({ data: { loading, error, cards } }) => {
-  if (loading) {
-    return <p>Loading ...</p>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
+class CardList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {reviewType: 'model'};
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  return (
-    <div>
-        {cards.map((name, index) => (
-          <Card key={index} card={name} />
-        ))}
-    </div>
-  );
+  handleChange(event) {
+    this.setState({reviewType: event.target.value});
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <Query query={UserQuery}>
+      {({ loading, error, data }) => {
+
+        if (loading) {
+          return <p>Loading ...</p>;
+        }
+        if (error) {
+          return <p>{error.message}</p>;
+        }
+
+        let cards = data.cards;
+        let includedCards = [];
+
+        switch (this.state.reviewType) {
+          case 'model':
+            includedCards = ['LookML Development'];
+          break;
+          case 'architecture':
+            includedCards = ['Application Servers (On-Premise)',
+                             'Application Database (On-Premise)',
+                             'Monitoring',
+                             'Data Security',
+                             'Data Pipeline'];
+          break;
+          case 'dataculture':
+            includedCards = ['User Enablement',
+                             'Content Management']
+          break;
+          case 'performance':
+            includedCards = ['Database Connections',
+                             'Performance',
+                             'Release Management']
+          break;
+          default:
+        }
+
+        cards = cards.filter(function(card, index, arr){
+          return (includedCards.indexOf(card.title) !== -1);
+        });
+
+        return (
+          <div>
+            <label htmlFor="reviewType">Choose Review:</label>
+            <select id="reviewType" value={this.state.reviewType} onChange={this.handleChange}>
+              <option value="model">Model</option>
+              <option value="architecture">On-Premise Architecture</option>
+              <option value="dataculture">Data Culture</option>
+              <option value="performance">Performance</option>
+            </select>
+            <div>
+                {cards.map((card, index) => (
+                  <Card key={card.title} card={card} />
+                ))}
+            </div>
+          </div>
+        )
+      }}
+      </Query>
+    )
+  }
 };
 
 CardList.propTypes = {
@@ -27,20 +88,21 @@ CardList.propTypes = {
 
 export const UserQuery = gql`
   query UserQuery {
-    cards (id:["TkEzzk6c"
-              ,"TbXzz78c"
-              ,"ckEzzkki"
-              ,"c5KnnjRi"
-              ,"iqKnn74T"
-              ,"ioEzzokT"
-              ,"ikEzzkeT"
-              ,"cAB66Egi"
-              ,"TqKnnjnc"
-              ,"ceXzzbRi"
-              ,"T5KnzGqc"
-              ,"c8e5bg7i"
-              ,"iGB6RkXT"
-              ,"TXrjjBpc"]) {
+    cards (id:["TkEzzk6c" # Database Connections
+              ,"TbXzz78c" # Data Pipeline
+              ,"ckEzzkki" # Data Security
+              ,"iqKnn74T" # Application Servers (On-Premise)
+              ,"ioEzzokT" # Application Database (On-Premise)
+              ,"ikEzzkeT" # Performance
+              ,"cAB66Egi" # Monitoring
+              ,"ceXzzbRi" # LookML Development
+              ,"TqKnnjnc" # Release Management
+#              ,"T5KnzGqc" # LookML Project Organization
+#              ,"c8e5bg7i" # LookML Explore Organization
+#              ,"iGB6RkXT" # LookML Model & View Organization
+              ,"TXrjjBpc" # Content Management
+              ,"c5KnnjRi" # User Enablement
+              ]) {
       title
       rows {
         rowId
@@ -55,6 +117,6 @@ export default graphql(UserQuery, {
   options: () => ({
     variables: {
     },
-    pollInterval: 10000,
+    pollInterval: 1000000,
   }),
 })(CardList);
